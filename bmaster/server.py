9ac527c94af776933b924538d1165738ea436de3
wumtdev <wumtdev@users.noreply.github.com>
2025-03-01 19:39:46 +0300

    configured uvicorn logging

--- bmaster/server.py
+++ bmaster/server.py
@@ -18,7 +18,13 @@ serving: Optional[Task] = None
 async def start():
 	global serving
 	logger.info("Starting uvicorn...")
-	config = uvicorn.Config(app, host="127.0.0.1", port=8000, loop="asyncio")
+	config = uvicorn.Config(
+		app,
+		host="127.0.0.1",
+		port=8000,
+		loop="asyncio",
+		log_config=None
+	)
 	server = uvicorn.Server(config)
 	serving = asyncio.create_task(server.serve())
 	logger.info("Uvicorn started")
