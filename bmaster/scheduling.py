8de8431149880e51cbb5c18d52ababa382959b01
wumtdev <wumtdev@users.noreply.github.com>
2025-03-01 18:38:14 +0300

    added scheduling config

--- bmaster/scheduling.py
+++ bmaster/scheduling.py
@@ -1,22 +1,29 @@
+from typing import Optional
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from pydantic import BaseModel
 
-from bmaster import logs
+from bmaster import configs, logs
 
+
+class SchedulingConfig(BaseModel):
+	url: str
+
+config: Optional[SchedulingConfig] = None
 
 logger = logs.logger.getChild('scheduling')
 
-jobstores = {
-	'default': SQLAlchemyJobStore(url='sqlite:///data/scheduler.db')
-}
-
 scheduler = AsyncIOScheduler(
-	jobstores=jobstores,
 	logger=logger.getChild('apscheduler')
 )
 
 async def start():
 	logger.info("Starting scheduler...")
+	config = SchedulingConfig.model_validate(configs.main_config['scheduling'])
+
+	scheduler.add_jobstore(
+		SQLAlchemyJobStore(url=config.url)
+	)
 	scheduler.start()
 	logger.info("Scheduler started")
 
