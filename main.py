4d5df13aadd2b7e9a8881c9aeab9ef3875e63b6e
wumtdev <wumtdev@users.noreply.github.com>
2025-03-01 19:46:59 +0300

    fixed async environment setup

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 import asyncio
 import sys
 import signal
+from typing import Optional
 
 import bmaster
 from bmaster.exc import StartError
 
 
 
-def cancel_loop(loop):
-	tasks = asyncio.all_tasks(loop)
-	for task in tasks: task.cancel()
-	loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
-	loop.close()
+# def cancel_loop(loop):
+# 	tasks = asyncio.all_tasks(loop)
+# 	for task in tasks: task.cancel()
+# 	loop.run_until_complete(asyncio.gather(*tasks, return_exceptions=True))
+# 	loop.close()
 
-async def start():
+main_task: Optional[asyncio.Task] = None
+async def main():
+	global main_task
 	loop = asyncio.get_running_loop()
+	main_task = asyncio.current_task(loop)
 	if sys.platform != "win32":
 		for sig in (signal.SIGINT, signal.SIGTERM):
-			loop.add_signal_handler(sig, lambda *_: cancel_loop(loop))
+			loop.add_signal_handler(sig, lambda *_: main_task.cancel())
 
 	print("main: starting bmaster...")
 	await bmaster.start()
 	print("main: bmaster started")
 
 	try: await loop.create_future()
-	except asyncio.CancelledError: await stop()
+	except asyncio.CancelledError: pass
 
-async def stop():
 	print("main: Stopping bmaster...")
 	await bmaster.stop()
 	print("main: Bmaster stopped")
 
-
 if __name__ == "__main__":
-	try: asyncio.run(start())
-	except KeyboardInterrupt: pass
+	try: asyncio.run(main())
 	except StartError: pass
 
 
