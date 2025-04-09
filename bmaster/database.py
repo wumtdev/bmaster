c456dd1e9fb033594e595df0daae16e52b2e92c6
wumtdev <wumtdev@users.noreply.github.com>
2025-04-09 23:26:40 +0300

    added TextArray database type decorator

--- bmaster/database.py
+++ bmaster/database.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import BaseModel
-from sqlalchemy import TypeDecorator, JSON
+from sqlalchemy import Text, TypeDecorator, JSON
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -62,3 +62,23 @@ class JSONModel(TypeDecorator):
 
 	def process_result_value(self, value: dict | None, dialect):
 		return self.pydantic_model.model_validate(value) if value is not None else None
+
+class TextArray(TypeDecorator):
+	impl = Text
+	
+	unique_set: bool
+
+	def __init__(self, unique_set: bool = False, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.unique_set = unique_set
+
+	def process_bind_param(self, value: list[str] | None, dialect):
+		# TODO: Escape ',' chars
+		return ','.join(value) if value is not None else None
+	
+	def process_result_value(self, value: str | None, dialect):
+		if not value: return None
+		res = value.split(',')
+		if self.unique_set:
+			res = set(res)
+		return res
