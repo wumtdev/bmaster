c28b51b660444159b689de80669b606e97b4c3a2
wumtdev <wumtdev@users.noreply.github.com>
2025-08-31 19:08:37 +0300

    removed lite from setup script

--- setup.py
+++ setup.py
@@ -9,51 +9,42 @@ import io
 BASE_PATH = Path("data")
 STATIC_PATH = Path("static")
 DEFAULT_CONFIG_FILE = Path("defaults/config.yml")
-DEFAULT_LITE_FILE = Path("defaults/lite.json")
 
 # Data Setup
 print(f"[-] Checking for directory: {BASE_PATH}...")
 
 if not BASE_PATH.exists() or not any(BASE_PATH.iterdir()):
-    BASE_PATH.mkdir(parents=True, exist_ok=True)
-    print(f"[+] Directory '{BASE_PATH}' checked/created.")
+	BASE_PATH.mkdir(parents=True, exist_ok=True)
+	print(f"[+] Directory '{BASE_PATH}' checked/created.")
 
-    (BASE_PATH / "sounds").mkdir(parents=True, exist_ok=True)
-    print(f"[+] Directory '{BASE_PATH / 'sounds'}' checked/created.")
+	(BASE_PATH / "sounds").mkdir(parents=True, exist_ok=True)
+	print(f"[+] Directory '{BASE_PATH / 'sounds'}' checked/created.")
 
-    # config.yml
-    config_path = BASE_PATH / "config.yml"
-    if not config_path.exists() and DEFAULT_CONFIG_FILE.exists():
-        config_data = yaml.safe_load(DEFAULT_CONFIG_FILE.read_text(encoding="utf-8"))
-        if "auth" in config_data and "jwt" in config_data["auth"]:
-            config_data["auth"]["jwt"]["secret_key"] = secrets.token_hex(32)
+	# config.yml
+	config_path = BASE_PATH / "config.yml"
+	if not config_path.exists() and DEFAULT_CONFIG_FILE.exists():
+		config_data = yaml.safe_load(DEFAULT_CONFIG_FILE.read_text(encoding="utf-8"))
+		if "auth" in config_data and "jwt" in config_data["auth"]:
+			config_data["auth"]["jwt"]["secret_key"] = secrets.token_hex(32)
 
-        with config_path.open("w", encoding="utf-8") as f:
-            yaml.safe_dump(
-                config_data,
-                f,
-                allow_unicode=True,
-                sort_keys=False,
-                default_flow_style=False,
-            )
-        print("[+] Config file 'config.yml' created.")
+		with config_path.open("w", encoding="utf-8") as f:
+			yaml.safe_dump(
+				config_data,
+				f,
+				allow_unicode=True,
+				sort_keys=False,
+				default_flow_style=False,
+			)
+		print("[+] Config file 'config.yml' created.")
 
-    # lite.json
-    lite_config_path = BASE_PATH / "lite.json"
-    if not lite_config_path.exists() and DEFAULT_LITE_FILE.exists():
-        lite_config_data = json.loads(DEFAULT_LITE_FILE.read_text(encoding="utf-8"))
-        with lite_config_path.open("w", encoding="utf-8") as f:
-            json.dump(lite_config_data, f, ensure_ascii=False, indent=2)
-        print("[+] Config file 'lite.json' created.")
-
-    # logs.log
-    logs_path = BASE_PATH / "logs.log"
-    if not logs_path.exists():
-        logs_path.touch()
-        print("[+] Log file 'logs.log' created.")
+	# logs.log
+	logs_path = BASE_PATH / "logs.log"
+	if not logs_path.exists():
+		logs_path.touch()
+		print("[+] Log file 'logs.log' created.")
 
 else:
-    print(f"[!] Directory '{BASE_PATH}' already exists and is not empty. Skipping...")
+	print(f"[!] Directory '{BASE_PATH}' already exists and is not empty. Skipping...")
 
 print("[+] Data directory and app data created")
 
@@ -62,17 +53,17 @@ print(f"[-] Checking for directory: {STATIC_PATH}...")
 STATIC_PATH.mkdir(parents=True, exist_ok=True)
 print(f"[+] Directory '{STATIC_PATH}' checked/created.")
 
-GITHUB_RELEASE_URL = "путь до репы"
+GITHUB_RELEASE_URL = "путь до последнего релиза(zip)"
 
 try:
-    print(f"[-] Downloading frontend build from {GITHUB_RELEASE_URL}...")
-    r = requests.get(GITHUB_RELEASE_URL, timeout=60)
-    r.raise_for_status()
+	print(f"[-] Downloading frontend build from {GITHUB_RELEASE_URL}...")
+	r = requests.get(GITHUB_RELEASE_URL, timeout=60)
+	r.raise_for_status()
 
-    with zipfile.ZipFile(io.BytesIO(r.content)) as z:
-        z.extractall(STATIC_PATH)
+	with zipfile.ZipFile(io.BytesIO(r.content)) as z:
+		z.extractall(STATIC_PATH)
 
-    print(f"[+] Frontend build extracted to '{STATIC_PATH}'.")
+	print(f"[+] Frontend build extracted to '{STATIC_PATH}'.")
 
 except Exception as e:
-    print(f"[!] Failed to download or extract frontend: {e}")
+	print(f"[!] Failed to download or extract frontend: {e}")
